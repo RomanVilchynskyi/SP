bcea4155cebfa5474bb2747660cd87dd297f2b54
Roman <VXlch_vg85@student.itstep.org>
2025-03-27 21:54:00 +0200

    02_ht

---
+++ 02_ht/Program.cs
@@ -0,0 +1,69 @@
+﻿internal class Program
+{
+    static bool isPrime = true;
+    static bool isFibonacci = true;
+    static void GeneratePrimes(int min, int max)
+    {
+        for (int num = min; num <= max && isPrime; num++)
+        {
+            if (IsPrime(num))
+                Console.WriteLine($"Prime - {num}");
+        }
+    }
+    static bool IsPrime(int num)
+    {
+        if (num < 2) return false;
+        for (int i = 2; i * i <= num; i++)
+        {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
+    static void GenerateFibonacci()
+    {
+        long a = 0, b = 1;
+        while (isFibonacci)
+        {
+            Console.WriteLine($"Fibonacci - {a}");
+            long temp = a + b;
+            a = b;
+            b = temp;
+            Thread.Sleep(500); 
+        }
+    }
+    private static void Main(string[] args)
+    {
+        Console.Write("Enter min: ");
+        string min = Console.ReadLine()!;
+        int min_;
+        if (string.IsNullOrEmpty(min))
+            min_ = 2;
+        else
+            min_ = int.Parse(min);
+
+
+        Console.Write("Enter max: ");
+        string max = Console.ReadLine()!;
+        int max_;
+        if (string.IsNullOrEmpty(max))
+            max_ = int.MaxValue;
+        else
+            max_ = int.Parse(max);
+
+        Thread prime = new Thread(() => GeneratePrimes(min_, max_));
+        Thread fibonacci = new Thread(GenerateFibonacci);
+
+        prime.Start();
+        fibonacci.Start();
+
+        Console.WriteLine("Enter any key");
+        Console.ReadKey();
+
+        isPrime = false;
+        isFibonacci = false;
+
+        prime.Join();
+        fibonacci.Join();
+    }
+}
---
+++ 02_task/MainWindow.xaml.cs
@@ -0,0 +1,71 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _02_task;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    static int[] numbers = new int[10000];
+    static int min, max;
+    static double average;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void GenerateNumbers()
+    {
+        Random rand = new Random();
+        for (int i = 0; i < numbers.Length; i++)
+            numbers[i] = rand.Next(1, 10001);
+    }
+
+    private void FindMin()
+    {
+        min = numbers.Min();
+    }
+
+    private void FindMax()
+    {
+        max = numbers.Max();
+    }
+
+    private void CalculateAverage()
+    {
+        average = numbers.Average();
+    }
+
+    private void StartCalculation(object sender, RoutedEventArgs e)
+    {
+        GenerateNumbers();
+
+        Thread min_ = new Thread(FindMin);
+        Thread max_ = new Thread(FindMax);
+        Thread avg_ = new Thread(CalculateAverage);
+
+        min_.Start();
+        max_.Start();
+        avg_.Start();
+
+        min_.Join();
+        max_.Join();
+        avg_.Join();
+
+        Application.Current.Dispatcher.Invoke(() =>
+        {
+            MessageBox.Show($"Min: {min}\nMax: {max}\nAverage: {average}");
+        });
+    }
+}
