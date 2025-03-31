c81fef65c469d91fb9bb39252c72e72805a04fef
Roman <VXlch_vg85@student.itstep.org>
2025-03-31 17:37:48 +0300

    ht_3

---
+++ 03_ht/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Threading;
+
+class FibonacciCounter
+{
+    private int n1 = 1, n2 = 1, evenCount = 0;
+    private object lock_ = new object();
+
+    public void CalcFibonacci()
+    {
+        for (int i = 1; i <= 10; i++)
+        {
+            lock (lock_)
+            {
+                int tmp = n1 + n2;
+                n1 = n2;
+                n2 = tmp;
+
+                if (tmp % 2 == 0)
+                    evenCount++;
+
+                Console.WriteLine($"{i,-10}{n1,-10}{n2,-10}{evenCount}");
+            }
+            Thread.Sleep(100);
+        }
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        FibonacciCounter counter = new FibonacciCounter();
+        Console.WriteLine("Iteration   n1        n2        evenCount");
+        Thread t1 = new Thread(counter.CalcFibonacci);
+        Thread t2 = new Thread(counter.CalcFibonacci);
+        Thread t3 = new Thread(counter.CalcFibonacci);
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+
+        t1.Join();
+        t2.Join();
+        t3.Join();
+    }
+}
---
+++ 03_task/Program.cs
@@ -0,0 +1,51 @@
+﻿internal class Program
+{
+    static int totalWords = 0;
+    static int totalLines = 0;
+    static int totalPunctuation = 0;
+    static object lockObject = new object();
+
+    static void AnalyzeFile(string filePath)
+    {
+        string text = File.ReadAllText(filePath);
+        int words = text.Split(new char[] { ' ', '\n', '\r', '\t' }).Length;
+        int lines = File.ReadAllLines(filePath).Length;
+        int punctuation = text.Count(c => ",.;:!?\"(){}[]<>/".Contains(c));
+
+        Interlocked.Add(ref totalWords, words);
+        Interlocked.Add(ref totalLines, lines);
+
+        lock (lockObject)
+        {
+            totalPunctuation += punctuation;
+        }
+    }
+    private static void Main(string[] args)
+    {
+        string directoryPath = @"C:\Users\MASTER\Desktop\test"; 
+        if (!Directory.Exists(directoryPath))
+        {
+            Console.WriteLine("Directory not found");
+            return;
+        }
+
+        string[] files = Directory.GetFiles(directoryPath, "*.txt");
+        Thread[] threads = new Thread[files.Length];
+
+        for (int i = 0; i < files.Length; i++)
+        {
+            string file = files[i];
+            threads[i] = new Thread(() => AnalyzeFile(file));
+            threads[i].Start();
+        }
+
+        foreach (Thread thread in threads)
+        {
+            thread.Join();
+        }
+
+        Console.WriteLine($"Total words: {totalWords}");
+        Console.WriteLine($"Total lines: {totalLines}");
+        Console.WriteLine($"Total punctuation marks: {totalPunctuation}");
+    }
+}
